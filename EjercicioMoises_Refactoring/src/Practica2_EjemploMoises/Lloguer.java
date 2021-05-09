2dd26fd36a1161c92f76ed994d3561292c2a2621
Axel-SC <axel1_97@hotmail.es>
2021-05-09 23:50:49 +0200

    Primer Commit: Creacion de clases

---
+++ EjercicioMoises_Refactoring/src/Practica2_EjemploMoises/Lloguer.java
@@ -0,0 +1,38 @@
+package Practica2_EjemploMoises;
+
+//Axel Sendros Cuenca
+
+import java.util.Date;
+
+public class Lloguer {
+
+	private Date data;
+	private int dies;
+	private Vehicle vehicle;
+
+	public Lloguer(Date data, int dies, Vehicle vehicleBasic) {
+		this.data = data;
+		this.dies = dies;
+		this.vehicle = vehicleBasic;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public int getDies() {
+		return dies;
+	}
+
+	public void setDies(int dies) {
+		this.dies = dies;
+	}
+
+	public Vehicle getVehicle() {
+		return this.vehicle;
+	}
+}
