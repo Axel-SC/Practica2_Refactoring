7c7ce9f99db295581502990d221f794a93d027bb
Axel-SC <axel1_97@hotmail.es>
2021-05-13 20:19:37 +0200

    Quinto Commit: Cambio variables locales(30 Abril)

---
+++ EjercicioMoises_Refactoring/src/Practica2_EjemploMoises/Practica2_EjemploMoises/Client.java
@@ -0,0 +1,104 @@
+package Practica2_EjemploMoises;
+
+//Axel Sendros Cuenca
+
+import java.util.Vector;
+
+public class Client {
+    private String nif;
+    private String nom;
+    private String telefon;
+    private Vector<Lloguer> lloguers;
+    private static final double EUROS_PER_UNITAT_DE_COST = 30;
+
+    public Client(String nif, String nom, String telefon) {
+        this.nif = nif;
+        this.nom = nom;
+        this.telefon = telefon;
+        this.lloguers = new Vector<Lloguer>();
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public void afegeix(Lloguer lloguer) {
+        if (!lloguers.contains(lloguer)) {
+            lloguers.add(lloguer);
+        }
+    }
+
+    public void elimina(Lloguer lloguer) {
+        if (lloguers.contains(lloguer)) {
+            lloguers.remove(lloguer);
+        }
+    }
+
+    public boolean conte(Lloguer lloguer) {
+        return lloguers.contains(lloguer);
+    }
+
+    public String informe() {
+        return  composaCapsalera() + 
+                composaDetall() + 
+                composaPeu();
+    }
+
+    public double importTotal() {
+        double total = 0;
+        for (Lloguer lloguer : lloguers) {
+            total += lloguer.quantitat() * EUROS_PER_UNITAT_DE_COST;
+        }
+        return total;
+    }
+
+    public int bonificacionsTotals() {
+        int bonificacions = 0;
+        for (Lloguer lloguer : lloguers) {
+            bonificacions += lloguer.bonificacions();
+        }
+        return bonificacions;
+    }
+
+    public String composaCapsalera() {
+        String resultat = "Informe de lloguers del client " + getNom() + " (" + getNif() + ")\n";
+        return resultat; 
+    }
+
+    public String composaDetall() {
+        String resultat = "";
+        for (Lloguer lloguer : lloguers) {
+            // composa els resultats d'aquest lloguer
+            resultat += "\t" + lloguer.getVehicle().getMarca() + " " + lloguer.getVehicle().getModel() + ": "
+                    + (lloguer.quantitat() * EUROS_PER_UNITAT_DE_COST) + "€" + "\n";
+        }
+        return resultat;
+    }
+
+    public String composaPeu() {
+        String resultat = "";
+        // afegeix informació final
+        resultat += "Import a pagar: " + importTotal() + "€\n" + "Punts guanyats: " + bonificacionsTotals() + "\n";
+        return resultat;
+    }
+}
